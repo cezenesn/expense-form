72d6ad8d64d57cea305f9261dbd6eca0c7f74068
cezenesn <cezene.neves@snlv.com.br>
2021-12-13 10:45:23 -0300

    saving more data

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-    const localData = localStorage.getItem("ls_nome");
-  const [newTitle, setNewTitle] = useState(localData);
-  const [newAmount, setNewAmount] = useState('');
-  const [newDate, setNewDate] = useState("");
+    const localTitle = localStorage.getItem("ls_nome");
+    const localAmount = localStorage.getItem("ls_amount");
+    const localDate = localStorage.getItem("ls_date");
+  const [newTitle, setNewTitle] = useState(localTitle);
+  const [newAmount, setNewAmount] = useState(localAmount);
+  const [newDate, setNewDate] = useState(localDate);
 
   const titleChangeHandler = (event) => {
     return setNewTitle(event.target.value);
@@ -25,19 +27,23 @@ const ExpenseForm = (props) => {
       date: new Date(newDate),
     };
     props.saveData(expenseData);
-    setNewTitle(localData);
-    setNewAmount("");
-    setNewDate("");
+    setNewTitle(localTitle);
+    setNewAmount(localAmount);
+    setNewDate(localDate);
   };
 
   const armazenar = (chave, valor) => {
     localStorage.setItem(chave, valor);
     setNewTitle(valor)
+    setNewAmount(localAmount)
+    setNewDate(localDate)
   };
 
   const deletar = (chave) => {
     localStorage.removeItem(chave);
     setNewTitle("")
+    setNewAmount('')
+    setNewDate('')
   };
   return (
     <form onSubmit={submitHandler}>
